95ae234b663a927a48a2a6644c0f60bcd006c79c
Marcelo Caetano <agcaetano13@gmail.com>
2026-09-01 19:44:59 -0300

    Initial commit: projeto planeja-api gerado pelo Spring Initializr

---
+++ src/main/java/io/github/agcaetano13/planeja/PlanejaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.agcaetano13.planeja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanejaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanejaApiApplication.class, args);
+	}
+
+}
